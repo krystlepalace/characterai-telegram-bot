c30c6a665f3dc7a61aa12533fbd54cd986520021
Fvbvke <novoselcevila12@gmail.com>
2023-09-16 23:39:44 +0300

    Utilite for running bot chat session for each user

--- utils/character.py
+++ utils/character.py
@@ -4,25 +4,32 @@ from config import CONFIG
 client = PyCAI(CONFIG.characterai_token.get_secret_value())
 client.start()
 
-char = input('Enter CHAR: ')
 
-chat = client.chat.get_chat(char)
+class Character():
+    _instances = {}
 
-participants = chat['participants']
+    def __init__(self, user_id):
+        self.chat = client.chat.new_chat(char="rFKvc0ejXz_X4w7hXDhrDjtkve0GTf-cuetnkImRCDQ")
 
-if not participants[0]['is_human']:
-    tgt = participants[0]['user']['username']
-else:
-    tgt = participants[1]['user']['username']
+    def __call__(cls, *args, **kwargs):
+        if kwargs["user_id"] not in cls._instances:
+            cls._instances[kwargs["user_id"]] = super(Character, cls).__call__(*args, **kwargs)
+            cls._instances[kwargs["user_id"]].__init__(*args, **kwargs)
+        return cls._instances[kwargs["user_id"]]
 
-while True:
-    message = input('You: ')
+    async def reply_to_bot(self, text):
+        participants = self.chat['participants']
 
-    data = client.chat.send_message(
-        chat['external_id'], tgt, message
-    )
+        if not participants[0]['is_human']:
+            tgt = participants[0]['user']['username']
+        else:
+            tgt = participants[1]['user']['username']
 
-    name = data['src_char']['participant']['name']
-    text = data['replies'][0]['text']
+        data = client.chat.send_message(
+                self.chat['external_id'], tgt, text
+            )
 
-    print(f"{name}: {text}")
+        name = data['src_char']['participant']['name']
+        text = data['replies'][0]['text']
+
+        return f"{name}: {text}"
