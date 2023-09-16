af75e4ebecc622538b0ed5650bf1ca88d4e18ba6
Fvbvke <novoselcevila12@gmail.com>
2023-09-17 00:28:10 +0300

    Fixing new bugs

--- utils/character.py
+++ utils/character.py
@@ -5,17 +5,19 @@ class Character:
     _instances = {}
 
     def __init__(self, user_id):
-        self.chat = client.chat.new_chat(
-            char="rFKvc0ejXz_X4w7hXDhrDjtkve0GTf-cuetnkImRCDQ")
+        self.user_id = user_id
 
     def __call__(cls, *args, **kwargs):
         if kwargs["user_id"] not in cls._instances:
-            cls._instances[kwargs["user_id"]] = super(Character, cls).__call__(*args, **kwargs)
+            cls._instances[kwargs["user_id"]] = super(Character, cls).__call__(
+                *args, **kwargs)
             cls._instances[kwargs["user_id"]].__init__(*args, **kwargs)
         return cls._instances[kwargs["user_id"]]
 
     async def reply_to_bot(self, text):
-        participants = self.chat['participants']
+        chat = await client.chat.new_chat(
+            char="rFKvc0ejXz_X4w7hXDhrDjtkve0GTf-cuetnkImRCDQ")
+        participants = chat['participants']
 
         if not participants[0]['is_human']:
             tgt = participants[0]['user']['username']
@@ -23,8 +25,8 @@ class Character:
             tgt = participants[1]['user']['username']
 
         data = await client.chat.send_message(
-                self.chat['external_id'], tgt, text
-            )
+            chat['external_id'], tgt, text
+        )
 
         name = data['src_char']['participant']['name']
         text = data['replies'][0]['text']
