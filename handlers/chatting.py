4b9700d6b83b3609202f7d00a039e390c0108a0f
Fvbvke <novoselcevila12@gmail.com>
2023-09-16 23:39:48 +0300

    Create chatting.py

---
+++ handlers/chatting.py
@@ -0,0 +1,13 @@
+from aiogram import Router, F
+from aiogram.types import Message
+from utils.character import Character
+
+router = Router()
+
+
+@router.message(F.text)
+async def char_reply(message: Message):
+    char = Character(message.from_user.id)
+    result = await char.reply_to_bot(message.text)
+
+    await message.reply(text=result)
