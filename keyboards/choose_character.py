dda93c0f35724fb14cb63985fbc4e2945c82d133
Fvbvke <novoselcevila12@gmail.com>
2023-09-17 17:29:58 +0300

    Added pagination

--- keyboards/choose_character.py
+++ keyboards/choose_character.py
@@ -1,21 +1,38 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from handlers.callbacks.factories import CharacterCallback, PaginationCallback
 
 # example of characters dict
 characters = {
     'Рик Санчез': "2aZlJFMrZvCUdu26Jvn-CUJ-wdcBCCfCPBwDysLyOyk",
     'Дио Брандо': "HZGKpemYKi0xEPaUCTkf_8H99MIoANY-vhvnFKPhwWw",
     'Мармок': "BB69lWJvnUwV4KBkzOmGiRyfSDkIlExCL3ltORDs2s4",
-    'Чоппер': "69DhQhcbx2h486NkVWDGKOL8KNCgg3zB04ddOYoGE7M"
+    'Чоппер': "69DhQhcbx2h486NkVWDGKOL8KNCgg3zB04ddOYoGE7M",
+    'Шако': "IQHlxKIJQN7aSX6-R8V1xFOWs03RAuYfQJAzYqVIFxM",
+    'Иван Золо': "uYw10Z_PyKv_fLruAQig0TIqescVmFu21ypSwX1e87Q",
+    'Уолтер Вайт': "IJ6eOickbcEbXeAn1Yl8buPo6MCy46BzFEoCIsTIKLU",
+    'Астольфо': "VIqELxEyIJttOJVzR_sYEDCOBq9OHuYBTmaEX97lFLw",
+    'Масюня': "mJcAJWqqJ7AUgeO79tND_mr6jAMQXrolRaSpqdCFa9I"
 }
+characters = list(characters.items())
+characters = [characters[i:i+5] for i in range(0, len(characters), 5)]
 
 
-def characters_builder():
+def characters_builder(page=0):
     builder = InlineKeyboardBuilder()
-    for name, char_id in characters.items():
+    for name, char_id in characters[page]:
         builder.button(
             text=name,
-            callback_data=char_id
+            callback_data=CharacterCallback(id=char_id).pack()
         )
-    builder.adjust(2)
+
+    builder.button(
+        text="<",
+        callback_data=PaginationCallback(page=page-1).pack()
+    )
+    builder.button(
+        text=">",
+        callback_data=PaginationCallback(page=page+1).pack()
+    )
+    builder.adjust(*[1 for i in range(len(characters[page]))], 2)
 
     return builder
