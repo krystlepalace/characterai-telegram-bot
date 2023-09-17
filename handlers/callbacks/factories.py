ba3150d19c08a7a074b76f2da47f8fd4f1bd7f56
Fvbvke <novoselcevila12@gmail.com>
2023-09-17 17:30:49 +0300

    Create factories.py
    Refactoring + CallbackData for pagination

---
+++ handlers/callbacks/factories.py
@@ -0,0 +1,9 @@
+from aiogram.filters.callback_data import CallbackData
+
+
+class CharacterCallback(CallbackData, prefix='char'):
+    id: str
+
+
+class PaginationCallback(CallbackData, prefix='page'):
+    page: int
